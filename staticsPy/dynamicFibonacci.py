12e04b5195ae4d84e49e0b8fecb23e9edb3b5972
mja123 <matias.aguilar.sansoni@gmail.com>
2022-07-05 12:06:34 -0300

    Starting with statics in python, dynamic Fibonacci

---
+++ staticsPy/dynamicFibonacci.py
@@ -0,0 +1,17 @@
+import re
+
+
+def fibonacci(num, memory = {}):
+    if ((num == 1) or (num == 0)):
+        return num
+    try:
+        return memory[num]
+    except KeyError:
+        result = fibonacci(num - 1, memory) + fibonacci(num - 2, memory)
+        memory[num] = result
+        print(f'num: {num}, memory: {memory}')
+        return result
+
+if (__name__ == '__main__'):
+    number = int(input("Choose the number to calculate: "))
+    print(fibonacci(number))
